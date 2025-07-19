09d9d3c4f9aa79e0bfc113cec2f3e807f141eb70
jyoti chetry <jyotichetry087@gmail.com>
2025-07-19 20:22:43 +0530

    tabs setup

---
+++ app/(tabs)/streak.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const streak = () => {
+  return (
+    <View>
+      <Text>streak</Text>
+    </View>
+  );
+};
+
+export default streak;
+
+const styles = StyleSheet.create({});
